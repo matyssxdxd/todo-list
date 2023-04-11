8cf3d53b46d8933815d0c175e3935d696c30f65b
matyss <matissp1302@gmail.com>
2023-04-11 19:53:58 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+try {
+     mongoose.connect("mongodb+srv://admin:Sviests1337@cluster0.amxz712.mongodb.net/todoDB");
+} catch (err) {
+    console.error(err);
+};
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = new mongoose.model("Item", itemSchema);
+
+app.get("/", (req, res) => {
+    try {
+        Item.find().then(item => {
+            res.render("list", {listTitle: "Today", listItem: item});
+        })
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+app.get("*", (req, res) => {
+    res.redirect("/");
+});
+
+app.post("/", (req, res) => {
+    const itemContent = req.body.newItem;
+    if (itemContent != "") {
+        const newItem = new Item({
+            name: itemContent
+        });
+        newItem.save();
+    }
+    res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const itemId = req.body.checkbox;
+    Item.findByIdAndDelete(itemId)
+    .catch(err => {
+        console.error(err);
+    });
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Sever started on port 3000.");
+});
